d29c99a09946a00adc1ec0f5f75e0a665a2f0c6b
Abraham_Kol <Abrahamkolawole87@gmail.com>
2021-02-05 22:56:36 +0100

    First commit

--- client/src/views/auth/Login.js
+++ client/src/views/auth/Login.js
@@ -34,15 +34,9 @@ function LoginPage({ login, isAuthenticated }) {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <div>
-              <img
-                className="mx-auto h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
               <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 Sign in to your account
               </h2>
-            
             </div>
             <form className="mt-8 space-y-6" onSubmit={(e) => onSubmit(e)}>
               <input type="hidden" name="remember" value="true" />
@@ -76,7 +70,7 @@ function LoginPage({ login, isAuthenticated }) {
                     className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                     placeholder="Password"
                     value={password}
- onChange={(e) => onChange(e)}
+                    onChange={(e) => onChange(e)}
                   />
                 </div>
               </div>
@@ -96,8 +90,6 @@ function LoginPage({ login, isAuthenticated }) {
                     Remember me
                   </label>
                 </div>
-
-              
               </div>
 
               <div>
