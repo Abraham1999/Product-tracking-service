4ff0c63f45b04aeaed1a26bb935c2fddb8c4c85c
Abraham_Kol <Abrahamkolawole87@gmail.com>
2021-02-06 15:22:19 +0100

    First commit

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,16 @@ import store from "./redux/store/store";
 import { loadUser } from "./redux/actions/auth";
 import setAuthToken from "./utils/setAuthToken";
 import { createBrowserHistory } from "history";
-import Landing from "./views/Landing/Landing";
-import Login from "./views/auth/Login";
-import Register from "./views/auth/Register";
-import Dashboard from "./views/Admin/Dashboard";
-import New from "./views/Admin/New"
-import Edit from "./views/Admin/Edit"
-import Delete from "./views/Admin/Delete"
-import All from "./views/Admin/All"
+// import Landing from "./views/Landing/Landing";
+// import Login from "./views/auth/Login";
+// import Register from "./views/auth/Register";
+// import Dashboard from "./views/Admin/Dashboard";
+// import New from "./views/Admin/New"
+// import Edit from "./views/Admin/Edit"
+// import Delete from "./views/Admin/Delete"
+// import All from "./views/Admin/All"
+
+import TestPage from "./views/Landing/TestPage";
 
 const hist = createBrowserHistory();
 
@@ -35,7 +37,7 @@ function App() {
     <div className="App">
       <Router history={hist}>
         <Switch>
-          <Route exact path="/" component={Landing} />
+          {/* <Route exact path="/" component={Landing} />
 
           <Route exact path="/login-page" component={Login} />
           <Route exact path="/register-page" component={Register} />
@@ -45,7 +47,9 @@ function App() {
           <PrivateRoute exact path="/edit/:id" component={Edit} />
           <PrivateRoute exact path="/all" component={All} />
           <PrivateRoute exact path="/delete" component={Delete} />
-         
+          */}
+
+          <Route exact path="/" component={TestPage} />
           <Redirect to="/" />
         </Switch>
       </Router>
